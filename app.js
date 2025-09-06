eecc908b070cf45b6dd13f89e0dd79b49fd23da9
rakesh <rakesh@sk3@gmail.com>
2025-09-06 15:08:58 +0530

    Add Node.js skeleton app with Docker & tests

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello from CI/CD Pipeline with Node.js! 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app; // for testing
